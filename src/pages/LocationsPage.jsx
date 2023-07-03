1f5c93357680ffe954d12e46482400f04d436e42
jlmunnoz <joseluis.munnoz@gmail.com>
2023-07-03 17:21:39 -0500

    modificado2

--- src/pages/LocationsPage.jsx
+++ src/pages/LocationsPage.jsx
@@ -1,11 +1,11 @@
-import { Link, useLoaderData, useLocation } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
-const LocationsPage = () => {
+function LocationsPage() {
 
     const dataLocations = useLoaderData();
 
     return (
-        <section className="py-20">
+        <section className="py-20 imagen_fondo">
             <div className="container px-2 mx-auto flex flex-col gap-6">
                 <h1 className="py-2 text-teal-500 font-extrabold text-3xl text-center transition-colors hover:text-amber-200">Ubicaciones</h1>
                 <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6">
@@ -18,16 +18,16 @@ const LocationsPage = () => {
                                 </Link>
                                 <h3 className="text-base font-semibold text-gray-200">Tipo: {type}</h3>
                                 <h4 className="text-base font-semibold text-gray-200">Dimensión: {dimension}</h4>
-                                <h5 className="text-base font-semibold text-gray-400">Creación: {created}</h5>                              
+                                <h5 className="text-base font-semibold text-gray-400">Creación: {created}</h5>
                                 {residents.length === 0 ? <h6 className="mt-auto text-amber-500 text-base font-bold">NO hay residentes (0)</h6> : <Link to={`residentes/${id}`} className="py-1 px-3 mt-auto rounded-2xl bg-teal-500 font-semibold self-start transition-colors hover:bg-amber-200 hover:text-sky-950">Total de Residentes: {residents.length}</Link>}
 
                             </div>
-                        )
+                        );
                     })}
                 </div>
             </div>
         </section>
     );
-};
+}
 
 export default LocationsPage;
