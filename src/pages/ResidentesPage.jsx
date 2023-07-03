7c946c2f513f8457c7c1d8c0b1ffe1089820d3ea
jlmunnoz <joseluis.munnoz@gmail.com>
2023-07-03 16:32:30 -0500

    se agrego el circulo amarillo para status desconocido

--- src/pages/ResidentesPage.jsx
+++ src/pages/ResidentesPage.jsx
@@ -20,7 +20,7 @@ const ResidentesPage = () => {
                 <p className="text-amber-200 font-extrabold text-3xl text-center transition-colors hover:text-teal-500">.:: {resident.name} ::.</p>
                 <Link to={'/ubicaciones'} className="py-2 px-3 mt-auto rounded-2xl bg-teal-500 font-semibold self-start transition-colors hover:bg-amber-200 hover:text-sky-950">REGRESAR</Link>
                 {/* parte dos */}
-                <div className="grid grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] gap-6">
+                <div className="grid grid-cols-[repeat(auto-fit,_minmax(280px,_1fr))] gap-6">
                     {resident.residents.map((element) => {
                         // console.log(element);                        
                         // console.log(i);
@@ -43,9 +43,12 @@ const ResidentesPage = () => {
                                 <p className="text-lg font-bold">Residente N° {i}</p>
                                 <h2 className="text-lg font-bold">Nombre: {habitante.name}</h2>
                                 <h3 className="text-base">Especie: {habitante.species}</h3>
-                                <h4 className="text-base">Género: {habitante.gebder}</h4>
-                                <Link to={element} className="text-base">{element}</Link>
-                                <img src={habitante.image} alt={habitante.image} width={128} height={128} className="object-cover"/>
+                                <h4 className="text-base">Género: {habitante.gender}</h4>
+                                <h5 className="text-base">Estado: {habitante.status} {habitante.status === 'Alive' ? '🟢' : habitante.status === 'Dead' ? '🔴': '🟡'} </h5>
+                                <Link to={element}><img src={habitante.image} alt={habitante.image} width={128} height={128} className="object-cover"/></Link>
+                                
+                                {/* <Link to={element} className="text-base">{element}</Link>
+                                <img src={habitante.image} alt={habitante.image} width={128} height={128} className="object-cover"/> */}
                             </div>
                         );
 
